0952ffa6c1b4aaefe5a09961694ad0344ee0aa62
Subash Shahi <subashsly33@gmail.com>
2017-07-22 10:13:00 +0545

    sdf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,6 @@ config.eager_load = true
 
 
 
-
-
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
