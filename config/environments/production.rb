b171fca64d3c5cf31273506eb73883cef610ee2f
Subash Shahi <subashsly33@gmail.com>
2017-07-22 10:17:08 +0545

    final

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ config.eager_load = true
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.serve_static_files = false
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
